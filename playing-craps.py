fb2d6d83c6dbe1bdd5c84591e0106394a680dc7e
DrSTaylor <105294873+DrSTaylor@users.noreply.github.com>
2022-05-22 16:55:34 -0500

    Update playing-craps.py

--- playing-craps.py
+++ playing-craps.py
@@ -1,49 +1,57 @@
+
+"""Roll two six-sided die 6,000,000 times."""
 import random
-for roll in range(10):
-  print(random.randrange(1,7), end-" ")
-  
-  """Roll two six-sided die 6,000,000 times."""
- import random
- 
- # face frequency counters
-  frequency1 = 0
-  frequency2 = 0
-  frequency3 = 0
-  frequency4 = 0
-  frequency5 = 0
-  frequency6 = 0
 
- # 6,000,000 die rolls
- for roll in range(6_000_000):  # note underscore separators
-     face = = random.randrange(1,7) + random.randrange(1,7)
+# face frequency counters
+frequency1 = 0
+frequency2 = 0
+frequency3 = 0
+frequency4 = 0
+frequency5 = 0
+frequency6 = 0
+frequency7 = 0
+frequency8 = 0
+frequency9 = 0
+frequency10 = 0
+frequency11 = 0
+frequency12 = 0
+frequencycraps = 0
+frequencywin = 0
+# 6,000,000 die rolls
+for roll in range(6_000_000):  # note underscore separators
+    face = random.randrange(1, 7) + random.randrange(1, 7)
 
-     # increment appropriate face counter
-     if face == 1:
-         frequency1 += 1
-     elif face == 2:
-         frequency2 += 1
-     elif face == 3:
-         frequency3 += 1
-     elif face == 4:
-         frequency4 += 1
-     elif face == 5:
-         frequency5 += 1
-     elif face == 6:
-         frequency6 += 1
-      elif face == 7:
-         frequency7 += 1
-      elif face == 8:
-         frequency8 += 1
-      elif face == 9:
-         frequency9 += 1
-      elif face == 10:
-         frequency10 += 1
-      elif face == 11:
-         frequency11 += 1
-      elif face == 12:
-         frequency12 += 1
+    # increment appropriate face counter
+    if face == 1:
+        frequency1 += 1
+    elif face == 2:
+        frequency2 += 1
+    elif face == 3:
+        frequency3 += 1
+    elif face == 4:
+        frequency4 += 1
+    elif face == 5:
+        frequency5 += 1
+    elif face == 6:
+        frequency6 += 1
+    elif face == 7:
+        frequency7 += 1
+    elif face == 8:
+        frequency8 += 1
+    elif face == 9:
+        frequency9 += 1
+    elif face == 10:
+        frequency10 += 1
+    elif face == 11:
+        frequency11 += 1
+    elif face == 12:
+        frequency12 += 1
+    
+        frequencycraps = frequency2 + frequency3 + frequency12
+   
+        frequencywin = frequency7 + frequency11
 
-print(f'Faces{"Frequency":>13}')
+print(f'Face{"Frequency":>13}')
 print(f'{1:>4}{frequency1:>13}')
 print(f'{2:>4}{frequency2:>13}')
 print(f'{3:>4}{frequency3:>13}')
@@ -56,9 +64,6 @@ print(f'{9:>4}{frequency9:>13}')
 print(f'{10:>4}{frequency10:>13}')
 print(f'{11:>4}{frequency11:>13}')
 print(f'{12:>4}{frequency12:>13}')
+print(f'{13:>4}{frequencycraps:>13}')
+print(f'{14:>4}{frequencywin:>13}')
 
-
-
-print(f'Outcome{"Frequency":>13}')
-print(f'{Craps:>4}{frequency2 + frequency3 + frequency12:>13}')
-print(f'{Win:>4}{frequency7 + frequency11:>13}')
